b9f2bc644b471f7de9a39bb5e57a984b131f1352
Toshiaki Asai <toshi.alternative@gmail.com>
2018-06-20 04:29:01 +0900

    defspellのためのユーティリティ

--- mikutter-utils.el
+++ mikutter-utils.el
@@ -30,5 +30,19 @@
     (and (string-match "Plugin\\(?:.\\|::\\)create\s*\\(?:(:\\([a-zA-Z0-9_]+\\))\\|:\\([a-zA-Z0-9_]+\\)\\)" bstr)
          (or (match-string 1 bstr) (match-string 2 bstr)))))
 
+;; ":twitter, :web" のようなシンボルを列挙した文字列から、対応するhuman readableな変数名を列挙した文字列にして返す。
+;; 基本的にはコロンを取り除くだけ("twitter, web")。
+;; 2個目以降の値が "1個目_" で始まってる場合は、その部分を取り除く(":twitter, :twitter_tweet" → "twitter, tweet")。
+(defun mikutter:defspell-arguments (symbol-listed-text)
+  (let* ((model-slugs (mapcar (lambda (x) (substring (string-trim x) 1))
+							  (split-string symbol-listed-text ",")))
+		 (prefix (format "%s_" (car model-slugs))))
+	(mapconcat 'identity
+			   (mapcar (lambda (x)
+						 (if (string-prefix-p prefix x)
+							 (substring x (length prefix))
+						   x))
+					   model-slugs) ", ")))
+
 (provide 'mikutter-utils)
 ;;; mikutter-utils.el ends here
